24746aeaf27c8c2ec05f67e41446c3479555b3dd
Hasham Malik <hasham2@gmail.com>
2014-04-24 18:56:25 +0500

    location changes

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,17 @@
 class Location < ActiveRecord::Base
+  
+  validates :address, presence: true
   validates :city, presence: true
+  validates :state, presence: true
+  validates :zip, presence: true
+  validates :country, presence: true
+  
+  geocoded_by :full_address, latitude: :lat, longitude: :lng
+  after_validation :geocode
+  
+  def full_address
+    ##convert address to geocoded values
+    "#{self.address}, #{self.city}, #{self.zip}, #{self.state}, #{self.country}"
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140424133237) do
+ActiveRecord::Schema.define(version: 20140424135033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,11 +33,11 @@ ActiveRecord::Schema.define(version: 20140424133237) do
     t.string   "zip"
     t.string   "country"
     t.boolean  "approx_only"
-    t.string   "lat"
-    t.string   "lng"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "timezone"
+    t.float    "lat"
+    t.float    "lng"
   end
 
   create_table "users", force: true do |t|
