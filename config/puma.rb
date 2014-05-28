d9893f1470c934f21a4a0a6b75c64724a56331fd
Hasham Malik <hasham2@gmail.com>
2014-05-28 16:27:07 +0500

    another change in puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,8 @@
 # inspired by https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server
-workers Integer(ENV['PUMA_WORKERS'] || 3)
+workers Integer(ENV['PUMA_WORKERS'] || 2)
 threads Integer(ENV['MIN_THREADS'] || 1), Integer(ENV['MAX_THREADS'] || 3)
 
-pidfile '/tmp/pids/puma.pid'
+#pidfile '/tmp/pids/puma.pid'
 bind 'unix:///tmp/stage.sock'
 
 daemonize
