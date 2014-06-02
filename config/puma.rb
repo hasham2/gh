2eb2ba0c01176fa0c7decc437709d50ad5f77b37
Hasham Malik <hasham2@gmail.com>
2014-06-02 19:23:34 +0500

    added pidfile path

--- config/puma.rb
+++ config/puma.rb
@@ -2,7 +2,7 @@
 workers Integer(ENV['PUMA_WORKERS'] || 2)
 threads Integer(ENV['MIN_THREADS'] || 1), Integer(ENV['MAX_THREADS'] || 3)
 
-#pidfile '/tmp/pids/puma.pid'
+pidfile 'tmp/pids/puma.pid'
 bind 'unix:///tmp/stage.sock'
 
 daemonize
