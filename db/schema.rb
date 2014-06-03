4ccbb676cd41603feba8c47f230c71829758bfe1
Atif <atif@stingpad3.(none)>
2014-06-03 18:45:33 +0500

    conflick resolved

--- db/schema.rb
+++ db/schema.rb
@@ -122,7 +122,6 @@ ActiveRecord::Schema.define(version: 20140603081715) do
     t.datetime "image_updated_at"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "session_id"
   end
 
   create_table "requirements", force: true do |t|
