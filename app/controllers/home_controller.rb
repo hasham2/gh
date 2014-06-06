6efb04a3c2d510affe880c0c800ca8689c25f1c2
Hasham Malik <hasham2@gmail.com>
2014-06-06 19:17:21 +0500

    removed auth from home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,4 @@
 class HomeController < ApplicationController
-	before_filter :authenticate_user!
-	before_action :check_enrollment	
   def index
     #@users = User.all
     @georesp = Geocoder.search(request.env['REMOTE_ADDR']) unless Rails.env.development?
