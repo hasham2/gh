613b3befb6e835df8d8e4084318e5201e7a15418
waleedarshad <waleedarshad72@gmail.com>
2014-06-19 15:18:34 +0500

    added edit view for job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -21,16 +21,66 @@ class JobsController < ApplicationController
 
 
   def show
-    @employer_job_id = params[:employer_job_id]
-    @job = Job.find(params[:id])
+    # @employer_job_id = params[:employer_job_id]
+     @employer_job_id = params[:employer_job_id]
+         @job = Job.find(params[:id])
+
+         @user_primary_photo = current_user.photos.where(:is_primary=>true)
+
+         # Total Views of a Job
+         @counter = @job.views  
+         @counter = @counter+1
+         @job.update_attributes(:views=>@counter)
+
+         # Active Jobs of Employer
+         @active_listings = current_user.jobs.where(:active_job=>true).count
+         # Total Jobs of Employer
+         @total_listings = current_user.jobs.count
   end
 
   def edit
-    @job = Job.find(params[:id])
-    session[:job_id] = @job.id
-    redirect_to job_steps_path  
-  end
+    @current_user_jobs = current_user.jobs.count
+     @job = Job.find(params[:id])
+     if current_user.employer == nil 
+      @business_name = ''
+    else
+      @business_name = current_user.employer.business_name
+      end 
+     if @job.location == nil
+      @job.build_location
+     else
+     @job.location
+     end
+    if @job.metrics.empty? 
+    5.times{@job.metrics.build}
+    else
+      if @job.metrics.count < 5
+      @m = 5 - @job.metrics.count
+      @m.times{@job.metrics.build}
+      end
 
+      @user_primary_photo = current_user.photos.where(:is_primary=>true)
+      @primary_photo = @job.photos.where(:is_primary => true)
+       if  @job.photos.empty?
+           @job.photos.build
+        end
+    end
+    # sesion[:job_id] = @job.id
+    # redirect_to job_steps_path  
+  end
+  def set_primary
+    binding.pry 
+  end
+  def update
+    binding.pry 
+    @job = Job.find(params[:id])
+    @done = @job.update_attributes(job_params)
+    if @done
+      flash[:notice]="Job Successfully updated"
+    else
+      flash[:notice]="Unable to update"
+    end
+  end
   def destroy
     @job = Job.find(params[:id])
     if @job.destroy
@@ -52,7 +102,9 @@ class JobsController < ApplicationController
 
 
   private
-  
+  def job_params
+  params.require(:job).permit!    
+  end  
   def is_employer
     unless current_user.role == 'employer'
       flash[:error] = "Access Dinied"
