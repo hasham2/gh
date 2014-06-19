01268f9a8eb2db7ad86b840681d3e0655ebc68f1
Hasham Malik <hasham2@gmail.com>
2014-06-19 11:38:03 +0500

    ignoring PostGIS for now

--- db/migrate/20140616143752_add_point_to_locations.rb
+++
@@ -1,8 +0,0 @@
-class AddPointToLocations < ActiveRecord::Migration
-  def change
-    change_table :locations do |t|
-      t.point :lonlat, geographic: true
-      t.index :lonlat, spatial: true
-    end
-  end
-end
