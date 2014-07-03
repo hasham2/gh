aadcf0ed72d1d8259e8588a0b1b0222790844dd2
Hasham Malik <hasham2@gmail.com>
2014-07-03 16:15:53 +0500

    leaflet images

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -42,7 +42,7 @@
 //= require turbolinks
 
 //= require_tree .
-
+L.Icon.Default.imagePath = '/assets/images';
 jQuery(function() {
    $('#user_certificate_list').tokenInput('./tags.json', {
     propertyToSearch: 'title'
